fbefae006dcab4eafa5683f2e23fd27fe5fb23a1
Adriano Machado <adrianoricardom@gmail.com>
2019-11-10 17:05:07 -0300

    estrutura do json aplicada

--- src/components/Insta.js
+++ src/components/Insta.js
@@ -4,37 +4,35 @@ import withStyles from "@material-ui/core/styles/withStyles";
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
 
-import { Redirect } from "react-router-dom";
 import BoxCompany from "./recursableComponents/BoxCompany";
 
 import Card from "@material-ui/core/Card";
-import CardHeader from "@material-ui/core/CardHeader";
 import CardMedia from "@material-ui/core/CardMedia";
-import CardContent from "@material-ui/core/CardContent";
 import CardActions from "@material-ui/core/CardActions";
-import Collapse from "@material-ui/core/Collapse";
-import Avatar from "@material-ui/core/Avatar";
+
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
 import { red } from "@material-ui/core/colors";
 import FavoriteIcon from "@material-ui/icons/Favorite";
 import ShareIcon from "@material-ui/icons/Share";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import MoreVertIcon from "@material-ui/icons/MoreVert";
 
 import logoSoma from "../img/logo_grupo_soma_PRETO.png";
 
 import { signIn } from "../actions";
+import { programacoes } from "../consts";
 
 const styles = theme => ({
   margin: {
-      margin:theme.spacing(1)
+    margin: theme.spacing(1)
     // maxWidth: 345,
   },
   media: {
     height: 400,
     width: 280
     // paddingTop: '56.25%', // 16:9
+  },
+  dateText: {
+    textAlign:'center'
   }
 });
 
@@ -49,8 +47,48 @@ class Insta extends React.Component {
       passwordError: false,
       recoveryError: false,
       recoverySubmitted: false,
-      recoveryShow: false
+      recoveryShow: false,
+      programacoes
     };
+  }
+  renderProgramacoes() {
+    const { programacoes, } = this.state;
+    const { classes } = this.props
+    return (
+        programacoes.map(data => {
+          return (
+            <Grid item direction="column">
+              <Typography variant="h5" className={classes.dateText} >{data.date}</Typography>
+              {this.renderProductsByDate(data)}
+            </Grid>
+          );
+        })
+    );
+  }
+
+  renderProductsByDate({ produtos, date }) {
+    const { classes } = this.props
+    return <Grid container direction="row" justify="center">
+              {produtos.map(produto => {    return                <Card className={classes.margin}>
+                    <CardMedia
+                      className={classes.media}
+                      image={produto.fotos[0].link}
+                      title="Produto"
+                    />
+
+                    <CardActions disableSpacing>
+                      <IconButton aria-label="add to favorites">
+                        <FavoriteIcon />
+                      </IconButton>
+                      <IconButton aria-label="share">
+                        <ShareIcon />
+                      </IconButton>
+                    </CardActions>
+                  </Card>
+
+              })}
+
+    </Grid>
   }
 
   render() {
@@ -67,29 +105,34 @@ class Insta extends React.Component {
         <BoxCompany insta={true} />
 
         <Container>
-            <div className={classes.margin}>
-          <Grid container direction="row" flexWrap="wrap">
-            {[0, 1, 2, 3].map(a => {
-              return (
-                <Card className={classes.margin}>
-                  <CardMedia
-                    className={classes.media}
-                    image="http://soma-images.somalabs.com.br/image/6000-produto_estilo-6000_1.jpg/300/500"
-                    title="Paella dish"
-                  />
+          <div className={classes.margin}>
+            <Grid container direction="row" flexWrap="wrap">
+              <Grid container direction='column' spacing={2}>
 
-                  <CardActions disableSpacing>
-                    <IconButton aria-label="add to favorites">
-                      <FavoriteIcon />
-                    </IconButton>
-                    <IconButton aria-label="share">
-                      <ShareIcon />
-                    </IconButton>
-                  </CardActions>
-                </Card>
-              );
-            })}
-          </Grid>
+              
+              {this.renderProgramacoes()}
+              </Grid>
+              {/* {this.state.programacoes.map(a => {
+                return (
+                  <Card className={classes.margin}>
+                    <CardMedia
+                      className={classes.media}
+                      image="http://soma-images.somalabs.com.br/image/6000-produto_estilo-6000_1.jpg/300/500"
+                      title="Produto"
+                    />
+
+                    <CardActions disableSpacing>
+                      <IconButton aria-label="add to favorites">
+                        <FavoriteIcon />
+                      </IconButton>
+                      <IconButton aria-label="share">
+                        <ShareIcon />
+                      </IconButton>
+                    </CardActions>
+                  </Card>
+                );
+              })} */}
+            </Grid>
           </div>
         </Container>
       </Fragment>
