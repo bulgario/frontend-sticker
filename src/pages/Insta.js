81fdb5e146ff0d1486948c2a489a976a9a88ddd2
Adriano Machado <adrianoricardom@gmail.com>
2020-01-30 00:51:08 -0300

    tentando mexer no estilo

--- src/pages/Insta.js
+++ src/pages/Insta.js
@@ -11,7 +11,7 @@ import Header from "../components/recursableComponents/Header";
 import Badge from "@material-ui/core/Badge";
 import Card from "@material-ui/core/Card";
 import CardMedia from "@material-ui/core/CardMedia";
-import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
+// import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
 
 import Typography from "@material-ui/core/Typography";
 
