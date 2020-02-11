7f192d064a4ba9ba1a7e2d9558beb4bdfeeaf8ae
Adriano Machado <adrianoricardom@gmail.com>
2020-02-11 19:03:00 -0300

    edit : commenting buttons

--- src/components/recursableComponents/Footer.js
+++ src/components/recursableComponents/Footer.js
@@ -3,12 +3,12 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
+// import Typography from '@material-ui/core/Typography';
+// import IconButton from '@material-ui/core/IconButton';
 import Fab from '@material-ui/core/Fab';
 
-import CommentIcon from '@material-ui/icons/Comment';
-import ShuffleIcon from '@material-ui/icons/Shuffle';
+// import CommentIcon from '@material-ui/icons/Comment';
+// import ShuffleIcon from '@material-ui/icons/Shuffle';
 
 import AddIcon from '@material-ui/icons/Add';
 
@@ -54,18 +54,18 @@ export default function BottomAppBar() {
           <CssBaseline />
           <AppBar position="fixed" className={classes.appBar}>
         <Toolbar>
-          <IconButton edge="start" color="inherit" aria-label="open drawer">
+          {/* <IconButton edge="start" color="inherit" aria-label="open drawer">
             <CommentIcon />
           </IconButton>
-          <Typography variant="body2" component="h6">RESUMO</Typography>
+          <Typography variant="body2" component="h6">RESUMO</Typography> */}
           <Fab color="primary" aria-label="add" className={classes.fabButton}>
             <AddIcon  className={classes.whiteButton}/>
           </Fab>
           <div className={classes.grow} />
-          <IconButton edge="start" color="inherit" aria-label="open drawer">
+          {/* <IconButton edge="start" color="inherit" aria-label="open drawer">
             <ShuffleIcon />
           </IconButton>
-          <Typography variant="body2" component="h6">AGRUPAR</Typography>
+          <Typography variant="body2" component="h6">AGRUPAR</Typography> */}
         </Toolbar>
       </AppBar>
         </React.Fragment>
